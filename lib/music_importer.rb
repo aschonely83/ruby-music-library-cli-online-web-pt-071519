2a9ea7afd97a1926d268b8241ded3df52b7d5e8c
Adam Schonely <aschonely@gmail.com>
2019-08-31 19:09:52 -0400

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -7,5 +7,12 @@ class MusicImporter
     
     def files
       Dir.entries(path).select {|path| path.match('.mp3')}
-    end  
+    end
+    
+    def import
+      files.each do |filename|
+        Song.create_from_filename(filename)
+      end    
+    end
+
   end  
--- lib/song.rb
+++ lib/song.rb
@@ -1,45 +1,55 @@
 class Song
-    extend Concerns::Findable
-    attr_accessor :name 
-    attr_reader :artist, :genre
+  extend Concerns::Findable
+  attr_accessor :name 
+  attr_reader :artist, :genre
     
-    @@all = []
+  @@all = []
     
-    def self.all
-      @@all
-    end
+  def self.all
+    @@all
+  end
     
-    def self.destroy_all
-      all.clear
-    end  
-    
-    def self.create(name)
-      new(name).save
-    end
-    
-     def self.new_by_filename(filename)
-     
-    end  
-    
-    def initialize(name, artist=nil, genre=nil)
-      @name = name
-      self.artist = artist
-      self.genre = genre
-    end
-    
-    def save
-      @@all << self
-      self
-    end
-    
-    def artist=(artist) 
-      @artist = artist
-      artist.add_song(self) if artist
-    end
-    
-    def genre=(genre) 
-      @genre = genre
-      genre.add_song(self) if genre
-    end
+  def self.destroy_all
+    all.clear
   end  
     
+  def self.create(name)
+    new(name).save
+  end
+
+  def self.new_from_filename(name)
+    song_name = name.split(" - ")[1]
+    artist_name = name.split(" - ")[0]
+    genre_name = name.split(" - ")[2].chomp(".mp3")
+
+    song = self.find_or_create_by_name(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.genre = Genre.find_or_create_by_name(genre_name)
+    song
+  end
+  
+  def self.create_from_filename(name)
+    @@all << self.new_from_filename(name)
+  end  
+
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+    self.artist = artist
+    self.genre = genre
+  end
+    
+  def save
+    @@all << self
+    self
+  end
+    
+  def artist=(artist) 
+    @artist = artist
+    artist.add_song(self) if artist
+  end
+    
+  def genre=(genre) 
+    @genre = genre
+    genre.add_song(self) if genre
+  end
+end
